b813f938a6f5ee25d964281100a5578760142985
Robert and Jenny <jenny_nickles@hotmail.com>
2021-01-21 23:05:17 -0800

    starter code

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -1,1 +1,30 @@
-// @TODO: YOUR CODE HERE!
+// Define the workspace area
+var svgArea = d3.select("body").select("svg");
+
+var svgWidth = window.innerWidth;
+var svgHeight = window.innerHeight;
+
+var margin = {
+  top: 50,
+  right: 50,
+  bottom: 50,
+  left: 50,
+};
+
+var height = svgHeight - margin.top - margin.bottom;
+var width = svgWidth - margin.left - margin.right;
+ append svg and group
+var svg = d3
+  .select(".chart")
+  .append("svg")
+  .attr("height", svgHeight)
+  .attr("width", svgWidth);
+
+var chartGroup = svg
+  .append("g")
+  .attr("transform", `translate(${margin.left}, ${margin.top})`);
+//* Access the data from somewhere and pull it into your program
+//Load data from data.csv
+d3.csv("data/data.csv").then(function (data) {
+  console.log(data);
+});
